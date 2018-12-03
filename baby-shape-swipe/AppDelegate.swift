59626fe28fca19803e762ef47a9f3cfae1e35a64
Steven Mycynek <smycynek@buildingengines.com>
2018-12-02 21:07:29 -0500

    load settings on startup

--- baby-shape-swipe/AppDelegate.swift
+++ baby-shape-swipe/AppDelegate.swift
@@ -5,6 +5,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SettingsManager.loadSettings()
+        SettingsManager.updateBuildInfo()
         return true
     }
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
